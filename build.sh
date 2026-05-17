9f0ffb66c1592b39bed287e008daf0cf5ca207f4
Hiba <hibahmer.2006@gmail.com>
2026-05-17 22:32:44 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', 'admin@opportunityhub.com', 'qXck01ptG')" | python manage.py shell
